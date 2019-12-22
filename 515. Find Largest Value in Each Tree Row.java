b3ca811ef0fe20f95d227896a3c58b099c6fe523
HuizeHuang <42952482+HuizeHuang@users.noreply.github.com>
2019-12-22 17:00:25 -0500

    Create 515. Find Largest Value in Each Tree Row.java

---
+++ 515. Find Largest Value in Each Tree Row.java
@@ -0,0 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    // Approach 1 - Level-order traversal - BFS - queue
+    // time - O(n)
+    // faster than 66.84%
+    public List<Integer> largestValues1(TreeNode root) { 
+        List<Integer> res = new ArrayList();
+        if (root == null) return res;
+        Queue<TreeNode> queue = new LinkedList();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            // level size
+            int size = queue.size();
+            // max value per level
+            int levelMax = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = queue.poll();
+                levelMax = Math.max(levelMax, curr.val);
+                // add new queue element
+                if (curr.left != null) queue.add(curr.left);
+                if (curr.right != null) queue.add(curr.right);
+            }
+            res.add(levelMax);
+        }
+        return res;
+    }
+    
+    // Approach 2 - DFS
+    // level order recursively
+    // time O(n) faster than 100%
+    public List<Integer> largestValues(TreeNode root) { 
+        List<Integer> res = new ArrayList();
+        if (root == null) return res;
+        helper(res, root, 0);
+        return res;
+    }
+    
+    public void helper(List<Integer> res, TreeNode root, int depth) {
+        if (root == null) return;
+        
+        if (res.size() == depth) 
+            res.add(root.val);
+        else        // change current level's value
+            res.set(depth, Math.max(root.val, res.get(depth)));
+        
+        helper(res, root.left, depth + 1);
+        helper(res, root.right, depth + 1);
+    }
+}
